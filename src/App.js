ce24068d05d7c61bf7d38f62fe6cc2b3e9c2952f
Suhwan Hwang <entercritical@gmail.com>
2020-07-01 22:18:01 +0900

    react movie app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import Home from './routes/Home';
+import './App.css'
+import { HashRouter, Route } from 'react-router-dom'
+import About from './routes/About'
+import Home from './routes/Home'
+import Detail from './routes/Detail'
+import Navigation from './components/Navigation'
+
+function App() {
+//  return <Home />
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
+}
+
+export default App;
