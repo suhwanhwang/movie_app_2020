564d716d1f62c907e923d88481edf59036e32ab4
Suhwan Hwang <entercritical@gmail.com>
2020-07-01 22:24:08 +0900

    Updates

---
+++ precache-manifest.4bd292ac1bec737ccae45337a6ce50a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3626c4f2ad685e93347192d6af079d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "067179e892cdea08be17",
+    "url": "/movie_app_2020/static/css/main.e50fc263.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067179e892cdea08be17",
+    "url": "/movie_app_2020/static/js/main.815ed66c.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
